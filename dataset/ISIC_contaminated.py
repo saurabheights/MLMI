b96ab9dc0e0fb605cee8a469619caaf09dfcd0f1
SAOSurvivor <sumantdeo46@gmail.com>
2020-01-24 02:06:50 +0100

    Made the following changes: 1. Added teh ISIC_contaminated file. This contains a class NoiseAdderDataset to add noise to the images at some intervals 2. Added the MNIST_contaminated which does the same as above for MNIST images

---
+++ dataset/ISIC_contaminated.py
@@ -0,0 +1,180 @@
+import math
+from pprint import pprint
+
+import numpy
+import torch
+import torchvision
+import csv
+from os import listdir
+import os
+from dataset.BaseDataset import BaseDataset
+import shutil
+
+
+class ISIC(BaseDataset):
+
+    def __init__(self,
+                 dataset_args,
+                 train_data_args,
+                 val_data_args):
+        super(ISIC, self).__init__(train_data_args, val_data_args)
+        dataset_dir = '/home/saosurvivor/Projects/MLMI/MLMI/data/' + self.__class__.__name__
+        #ToDo - This is CIFAR code copy pasted. Fix it.
+        # ToDo - Fix mean and std.
+        mean = (0.5, 0.5, 0.5)
+        std = (0.5, 0.5, 0.5)
+        self.__normalize_transform = torchvision.transforms.Compose(
+            [torchvision.transforms.ToTensor(),
+             torchvision.transforms.Normalize(mean, std)])
+
+        # Normalization transform does (x - mean) / std
+        # To denormalize use mean* = (-mean/std) and std* = (1/std)
+        self.denormalization_transform = torchvision.transforms.Normalize((-1, -1, -1), (2, 2, 2))
+
+        self.original_training_set = torchvision.datasets.ImageFolder(root=dataset_dir,
+                                                                  transform=self.__normalize_transform)
+
+        noise_adder = NoiseAdderDataset(self.original_training_set)
+        self.original_training_set = noise_adder.set_noise(noise=3, interval=1000)
+
+        # Split train data into training and cross validation dataset using 9:1 split ration
+        split_ratio = 0.9
+        self.trainset, self.validationset = self._uniform_train_val_split(split_ratio)
+        self.full_training_set = self.trainset
+
+        if dataset_args.get('training_subset_percentage'):
+            training_subset_percentage = dataset_args['training_subset_percentage']
+            self.trainset = self.get_uniform_subset_by_percentage(training_subset_percentage)
+
+        # self.testset = torchvision.datasets.CIFAR10(root=dataset_dir,
+        #                                             train=False,
+        #                                             download=True,
+        #                                             transform=self.__normalize_transform)
+
+    def debug(self):
+        # get some random training images
+        data_iter = iter(self.train_dataloader)
+        print(type(data_iter))
+        images, labels = data_iter.next()
+
+        # show images
+        super(ISIC, self).imshow(torchvision.utils.make_grid(images))
+
+        # print labels
+        pprint(' '.join('%s' % self.classes[labels[j]] for j in range(len(images))))
+
+    def denormalize(self, x):
+        return self.denormalization_transform(x)
+
+    @property
+    def classes(self):
+        return ['MEL', 'NV', 'BCC', 'AKIEC', 'BKL', 'DF', 'VASC']
+
+    def get_uniform_subset(self, samples_per_label):
+        targets = self.original_training_set.targets
+        if type(targets) == list:
+            targets = numpy.array(targets)
+            labels = targets
+        elif type(targets) == torch.tensor or type(targets) == torch.Tensor:
+            labels = targets.numpy()
+
+        indices = []
+        for i in range(len(self.classes)):
+            label_indices = numpy.argwhere(labels == i)
+            label_indices = label_indices[:samples_per_label]
+            indices.extend(label_indices.squeeze().tolist())
+
+        uniform_subset = torch.utils.data.Subset(self.original_training_set, indices)
+        return uniform_subset
+
+    def get_uniform_subset_by_percentage(self, training_subset_percentage):
+        labels = self.get_subset_targets(self.full_training_set)
+        indices = []
+        for label_value in range(len(self.classes)):
+            label_indices = numpy.argwhere(labels == label_value)
+            number_of_labels = int(math.floor(training_subset_percentage * len(label_indices) + 0.5))
+            label_indices = label_indices[:number_of_labels]
+            indices.extend(label_indices.squeeze().tolist())
+
+        uniform_subset = torch.utils.data.Subset(self.full_training_set, indices)
+        return uniform_subset
+
+    def csv_loader(self):
+        label_path = '/home/saosurvivor/Projects/MLMI/MLMI/data/' + self.__class__.__name__ +\
+                     "/labels/ISIC2018_Task3_Training_GroundTruth.csv"
+        dataset_dir = '/home/saosurvivor/Projects/MLMI/MLMI/data/' + self.__class__.__name__
+        with open(label_path, 'r') as f:
+            reader = csv.reader(f)
+            line_count = 0
+            for row in reader:
+                if line_count==0:
+                    print(f'Column names are {", ".join(row)}')
+                    line_count += 1
+                else:
+                    print(f'row names are {", ".join(row)}')
+                    fileName = dataset_dir + "/images/" + row[0] + ".jpg"
+                    path_label = ""
+                    if os.path.exists(fileName):
+                        for i in range(1,8):
+                            if (row[i] == '1.0'):
+                                path_label = self.classes[i-1]
+                                break
+                        path_label = dataset_dir+ "/images/" + path_label
+                        if os.path.exists(path_label) == False:
+                            os.mkdir(path_label)
+                        shutil.move(fileName, path_label)
+                    line_count += 1
+                    print(fileName)
+                    print("copied to")
+                    print(path_label)
+
+
+class NoiseAdderDataset:
+
+    def __init__(self, dataset):
+        self.dataset = dataset
+
+    def get_item(self, item):
+        image, label = self.dataset[item]
+        return image, label
+
+    def get_size_dataset(self):
+        return len(self.dataset)
+
+    def set_noise(self, noise, interval):
+        data = []
+        for i in range(self.get_size_dataset()):
+            image, label = self.get_item(i)
+            noise_flag = False
+            if i % interval == 0:
+                # TODO : Add noise with the "noise" variable
+                print(i)
+                noise_flag = True
+            data.append([image, label, noise_flag])
+
+        return data
+
+
+def main():
+
+    dataset_args = dict(
+        name=ISIC,
+        training_subset_percentage=80,
+        # For Data Inflation Study - Set to None to use full dataset
+    )
+    train_data_args = dict(
+        batch_size=64,
+        shuffle=True,
+        to_train=True,
+    )
+    val_data_args = dict(
+        batch_size=256,
+        shuffle=False,
+        validate_step_size=1,
+    )
+    dataset = ISIC(dataset_args, train_data_args, val_data_args)
+    dataset.debug()
+
+if __name__== "__main__":
+  main()
+
---
+++ dataset/MNIST_contaminated.py
@@ -0,0 +1,142 @@
+import math
+from pprint import pprint
+
+import numpy
+import torch
+import torchvision
+from torch.utils.data import DataLoader
+
+from dataset.BaseDataset import BaseDataset
+from utils import logger
+
+
+class MNISTContaminated(BaseDataset):
+    """
+    Loads the train/validation/test set.
+    Every image in the dataset is 28x28 pixels and the labels are numbered from 0-9
+    for A-J respectively.
+    Split of Train is done with 9:1 to generate training and validation dataset
+    """
+
+    def __init__(self,
+                 dataset_args,
+                 train_data_args,
+                 val_data_args):
+        super(MNISTContaminated, self).__init__(train_data_args, val_data_args)
+
+        dataset_dir = './data/' + self.__class__.__name__
+
+        self.mean = dataset_args.get('mean', (0.5,))
+        self.std = dataset_args.get('std', (0.5,))
+
+        logger.info(f'Mean and Std used: {self.mean}, {self.std}')
+
+        self.demean = [-m / s for m, s in zip(self.mean, self.std)]
+        self.destd = [1 / s for s in self.std]
+
+        self.normalize_transform = self.get_normalize_transform()
+
+        # Normalization transform does (x - mean) / std
+        # To denormalize use mean* = (-mean/std) and std* = (1/std)
+        self.denormalization_transform = torchvision.transforms.Normalize(self.demean, self.destd)
+
+        self.original_training_set = torchvision.datasets.MNIST(root=dataset_dir,
+                                                                train=True,
+                                                                download=True,
+                                                                transform=self.normalize_transform)
+
+        noise_adder = NoiseAdderDataset(self.original_training_set)
+        self.original_training_set = noise_adder.set_noise(noise=3, interval=1000)
+
+        # Split train data into training and cross validation dataset using 9:1 split ration
+        split_ratio = 0.9
+        self.trainset, self.validationset = self._uniform_train_val_split(split_ratio)
+        self.full_training_set = self.trainset
+
+        if dataset_args.get('training_subset_percentage'):
+            training_subset_percentage = dataset_args['training_subset_percentage']
+            self.trainset = self.get_uniform_subset_by_percentage(training_subset_percentage)
+
+        self.testset = torchvision.datasets.MNIST(root=dataset_dir,
+                                                  train=False,
+                                                  download=True,
+                                                  transform=self.normalize_transform)
+
+    def debug(self):
+        # get some random training images
+        data_iter = iter(self.train_dataloader)
+        images, labels = data_iter.next()
+
+        # show images
+        super(MNISTContaminated, self).imshow(torchvision.utils.make_grid(images))
+
+        # print labels
+        pprint(' '.join('%s' % self.classes[labels[j]] for j in range(len(images))))
+
+    def get_normalize_transform(self):
+        normalize_transform = torchvision.transforms.Compose(
+            [torchvision.transforms.Scale(32),
+             torchvision.transforms.ToTensor(),
+             torchvision.transforms.Normalize(self.mean, self.std)])
+        return normalize_transform
+
+    def denormalize(self, x):
+        return self.denormalization_transform(x)
+
+    @property
+    def classes(self):
+        return [str(i) for i in range(10)]
+
+    def get_uniform_subset(self, samples_per_label):
+        targets = self.original_training_set.targets
+        if type(targets) == list:
+            targets = numpy.array(targets)
+            labels = targets
+        elif type(targets) == torch.tensor or type(targets) == torch.Tensor:
+            labels = targets.numpy()
+
+        indices = []
+        for i in range(len(self.classes)):
+            label_indices = numpy.argwhere(labels == i)
+            label_indices = label_indices[:samples_per_label]
+            indices.extend(label_indices.squeeze().tolist())
+
+        uniform_subset = torch.utils.data.Subset(self.original_training_set, indices)
+        return uniform_subset
+
+    def get_uniform_subset_by_percentage(self, training_subset_percentage):
+        labels = self.get_subset_targets(self.full_training_set)
+        indices = []
+        for label_value in range(len(self.classes)):
+            label_indices = numpy.argwhere(labels == label_value)
+            number_of_labels = int(math.floor(training_subset_percentage * len(label_indices) + 0.5))
+            label_indices = label_indices[:number_of_labels]
+            indices.extend(label_indices.squeeze().tolist())
+
+        uniform_subset = torch.utils.data.Subset(self.full_training_set, indices)
+        return uniform_subset
+
+class NoiseAdderDataset:
+
+    def __init__(self, dataset):
+        self.dataset = dataset
+
+    def get_item(self, item):
+        image, label = self.dataset[item]
+        return image, label
+
+    def get_size_dataset(self):
+        return len(self.dataset)
+
+    def set_noise(self, noise, interval):
+        data = []
+        for i in range(self.get_size_dataset()):
+            image, label = self.get_item(i)
+            noise_flag = False
+            if i % interval == 0:
+                # TODO : Add noise with the "noise" variable
+                print(i)
+                noise_flag = True
+            data.append([image, label, noise_flag])
+
+        return data
