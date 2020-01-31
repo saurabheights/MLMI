784a2b9dacfae0eddc3aa59c7c1911933e43075a
SAOSurvivor <sumantdeo46@gmail.com>
2020-01-31 02:09:14 +0100

    GanTrainer modification

--- GanTrainer.py
+++ GanTrainer.py
@@ -249,7 +249,7 @@ def main():
             evaluation_classifier_mean=(0.5 ,0.5 , 0.5),
             inception_metric=dict(
                 evaluation_arch_name='models.classification.ConvNetSimple.ConvNetSimple',
-                evaluation_classifier_weights='logs/2019-12-27T13:09:07.398172_mode_classification_model_ConvNetSimple_dataset_MNIST_subset_1.0_bs_64_name_Adam_lr_0.001_weight_decay_0.005/epoch_0034-model-val_accuracy_98.06859806859806.pth',
+                evaluation_classifier_weights='logs/2020-01-30T19:52:17.106300_train_mode_classification_model_ConvNetSimple_dataset_ISIC_subset_1.0_bs_64_name_Adam_lr_1e-05/epoch_0003-model-val_accuracy_66.76616915422886.pth',
                 classifier_model_layer=4,
                 evaluation_size=10000,
                 evaluation_classifier_std=(0.5,),
