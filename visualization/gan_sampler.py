b68386af6e5758c2557a65d2c1ea0165941fdcc0
SAOSurvivor <sumantdeo46@gmail.com>
2020-02-05 01:17:15 +0100

    changed the visualization

--- visualization/gan_sampler.py
+++ visualization/gan_sampler.py
@@ -45,7 +45,7 @@ class GanSampler(Callbacks):
         with torch.no_grad():
             images = generator(noise).detach()
             images = images.cpu()
-            images = images.reshape(-1, 1, 64, 64)
+            images = images.reshape(-1, 3, 64, 64)
             images = images.mul(0.5).add(0.5)
             image = torchvision.utils.make_grid(images, nrow=4)  # ToDo Remove hardcoded value
 
